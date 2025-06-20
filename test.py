71b673f630861c29d65c4e3ddfc06787ca4b8bbd
SamuelRolands <samuelrolands27@gmail.com>
2025-06-20 23:28:27 +0530

    test

---
+++ test.py
@@ -0,0 +1,6 @@
+import fastf1
+session = fastf1.get_session(2023, 'Monza', 'Q')
+session.load()
+laps = session.laps.pick_driver('VER')  # Verstappen
+lap = laps.pick_fastest()
+telemetry = lap.get_car_data()
